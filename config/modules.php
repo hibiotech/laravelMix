2ced67e9d96ebd7e9da1301c8805b9e82d5926f2
hibiotech <dr.eric1125@gmail.com>
2021-08-10 17:54:49 +0200

    uppdated template route

--- config/modules.php
+++ config/modules.php
@@ -38,11 +38,13 @@ return [
             'assets/js/app' => 'Resources/assets/js/app.tsx',
             'assets/js/bootstrap' => 'Resources/assets/js/bootstrap.js',
             'assets/js/app/index' => 'Resources/assets/js/app/index.tsx',
+
             'assets/js/app/components/App' => 'Resources/assets/js/app/components/App.tsx',
-            'assets/js/app/components/Router' => 'Resources/assets/js/app/pages/Router.tsx',
+            'assets/js/app/components/AppRouter' => 'Resources/assets/js/app/components/AppRouter.tsx',
+
             'assets/js/app/pages/Page1' => 'Resources/assets/js/app/pages/Page1.tsx',
             'assets/js/app/pages/Page2' => 'Resources/assets/js/app/pages/Page2.tsx',
-            
+
             'assets/sass/app' => 'Resources/assets/sass/app.scss',
             // 'webpack' => 'webpack.mix.js',
             // 'package' => 'package.json',
