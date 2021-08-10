c04bbb01eaf2f76da2a6dc9f173b4486f5bd2af3
hibiotech <dr.eric1125@gmail.com>
2021-08-10 23:06:15 +0200

    Added an first demo module

---
+++ Modules/Demo/Resources/assets/js/app/pages/Page1.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+export const Page1 = () => {
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <h1>Test demo1!</h1>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default Page1;
---
+++ Modules/Demo/Resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('demo::layouts.master')
+
+@section('content')
+    <div id="app"></div>
+@endsection
