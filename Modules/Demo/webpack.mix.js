608d90ccc98d6a4374a61462694efc3e5fbb8c39
hibiotech <dr.eric1125@gmail.com>
2021-08-11 10:34:40 +0200

    fixed sass route in local mix file

--- Modules/Demo/webpack.mix.js
+++ Modules/Demo/webpack.mix.js
@@ -7,7 +7,7 @@ require('laravel-mix-merge-manifest');
 mix.setPublicPath('../../public').mergeManifest();
 
 mix.ts(__dirname + '/Resources/assets/js/app.tsx', 'js/demo.js').react();
-mix.sass("resources/css/index.scss", "public/css/demo.css");
+mix.sass("/Resources/assets/sass/app.scss", "public/css/demo.css");
 
 if (mix.inProduction()) {
     mix.version();
